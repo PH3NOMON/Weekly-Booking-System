821a465e4df919dee6021b98006b425460fbd4d7
Simon Shrestha <120302946+PH3NOMON@users.noreply.github.com>
2023-03-08 11:46:04 +0545

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import Labs from './App';
+import Labs from './components/Labs';
+
+
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Labs />
+   <Labs />
+   
+   
   </React.StrictMode>
 );
 
